3db86d435ccc30c9880d74dbbc95b36fb8d25967
Rodrigo Sebastião Moreira Lima <rodrigomoreirtiads@gmail.com>
2025-09-22 15:17:00 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Configuração do Firebase
+// IMPORTANTE: Substitua os valores abaixo pelas suas credenciais do Firebase
+
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY_AQUI",
+    authDomain: "SEU_PROJECT_ID.firebaseapp.com",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_PROJECT_ID.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID"
+};
+
+// Exportar configuração
+window.firebaseConfig = firebaseConfig;
+
